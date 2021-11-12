df8a5ba2766a1784e43f34d21bc9e16a1705baaa
ioan <55089021+IOANSTIRBU@users.noreply.github.com>
2021-11-12 11:01:49 +0100

    init commit

--- src/main/java/com/example/springpatterns/patterns/behavioral/template/HtmlHome.java
+++ src/main/java/com/example/springpatterns/patterns/behavioral/template/HtmlHome.java
@@ -4,22 +4,12 @@ package com.example.springpatterns.patterns.behavioral.template;
 public class HtmlHome extends HtmlAboutUs {
     @Override
     public String head() {
-        return """
-                <!DOCTYPE html>
-                <html lang="en" xmlns:th="http://www.thymeleaf.org">
-                <head>
-                    <meta charset="UTF-8">
-                    <title>Página de inicio</title>
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-                </head><body>
-                """;
+        return "Pagina de Inicio";
     }
 
     @Override
     public String body() {
-        return """
-                <h1> Hola desde Home </h1>
-                """;
+        return "Hola desde home";
     }
 
 }
