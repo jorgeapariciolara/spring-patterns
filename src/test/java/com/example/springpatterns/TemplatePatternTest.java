92074776d833a60acf698d69581a73398db0a2f7
alansastre <alan@alansastre.co>
2021-11-12 10:09:12 +0100

    Initial commit

---
+++ src/test/java/com/example/springpatterns/TemplatePatternTest.java
@@ -0,0 +1,36 @@
+package com.example.springpatterns;
+
+import com.example.springpatterns.patterns.behavioral.template.HtmlAboutUs;
+import com.example.springpatterns.patterns.behavioral.template.HtmlHome;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TemplatePatternTest {
+
+    @Test
+    void htmlAbout() {
+
+        HtmlAboutUs about = new HtmlAboutUs();
+        assertTrue(about.head().contains("Nuestro equipo"));
+        assertTrue(about.body().contains("Página Nosotros"));
+        assertTrue(about.footer().contains("Copyright 2021 Principal")); // heredado
+        // página global
+        assertTrue(about.render().contains("Nuestro equipo"));
+        assertTrue(about.render().contains("Página Nosotros"));
+        assertTrue(about.render().contains("Principal"));
+    }
+
+    @Test
+    void htmlHome() {
+
+        HtmlHome htmlHome = new HtmlHome();
+        String head = htmlHome.head();
+        assertTrue(head.contains("Página de inicio"));
+        assertTrue(htmlHome.body().contains("Home"));
+        assertTrue(htmlHome.footer().contains("Copyright 2021 Principal")); // heredado
+        // página global
+        assertTrue(htmlHome.render().contains("Página de inicio"));
+        assertTrue(htmlHome.render().contains("Home"));
+        assertTrue(htmlHome.render().contains("Principal"));
+    }
+}
