b6023382270803a4c4f55aa6b5ad4e3d9eedbbdf
David Sans <dsans79@yahoo.es>
2021-11-12 10:59:56 +0100

    pom update build succes Ok

---
+++ src/test/java/com/example/springpatterns/patterns/structural/decorator/PhotoTest.java
@@ -0,0 +1,49 @@
+package com.example.springpatterns.patterns.structural.decorator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PhotoTest {
+
+    @Test
+    void show() {
+        //Photo photo = new Photo();
+    }
+
+    @Test
+    void cost() {
+    }
+
+    @Test
+    void getBrightness() {
+    }
+
+    @Test
+    void getContrast() {
+    }
+
+    @Test
+    void getSaturation() {
+    }
+
+    @Test
+    void getBlur() {
+    }
+
+    @Test
+    void setBrightness() {
+    }
+
+    @Test
+    void setContrast() {
+    }
+
+    @Test
+    void setSaturation() {
+    }
+
+    @Test
+    void setBlur() {
+    }
+}
