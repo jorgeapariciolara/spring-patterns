e68c3707f5f10a2c24e928bd404c0ef2052c184e
Ismael Gonzalez <ismamontiel@hotmail.com>
2021-11-12 12:11:06 +0100

    pom and maven

---
+++ src/test/java/com/example/springpatterns/patterns/structural/adapter/CarTest.java
@@ -0,0 +1,25 @@
+package com.example.springpatterns.patterns.structural.adapter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void speedUp() {
+    }
+
+    @Test
+    void getSpeed() {
+    }
+
+    @Test
+    void setSpeed() {
+    }
+}
