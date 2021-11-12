d710ebd34013e658e34456ed0b222c6fdfd0a3e5
David Sans <dsans79@yahoo.es>
2021-11-12 11:55:13 +0100

    add decorator test

--- src/test/java/com/example/springpatterns/patterns/structural/decorator/PhotoTest.java
+++ src/test/java/com/example/springpatterns/patterns/structural/decorator/PhotoTest.java
@@ -1,49 +1,85 @@
 package com.example.springpatterns.patterns.structural.decorator;
 
+import com.example.springpatterns.patterns.structural.decorator.decoradores.EffectRetro;
+import com.example.springpatterns.patterns.structural.decorator.decoradores.EffectVintage;
+import com.example.springpatterns.patterns.structural.decorator.decoradores.PhotoDecorator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhotoTest {
+@BeforeEach
+void setup(){
+
+}
 
     @Test
-    void show() {
-        //Photo photo = new Photo();
+    void showRetroTest() {
+        Photo selfie = new DigitalPhoto();
+        Photo  selfieRetro = new EffectRetro(selfie);
+       assertEquals("Normal photo without edit + Retro",selfieRetro.show());
+        double precio=selfieRetro.cost();
+        assertEquals(35,precio);
+
     }
 
     @Test
-    void cost() {
+    void costBaseTest() {
+        Photo selfie = new DigitalPhoto();
+        double precio=selfie.cost();
+        assertEquals(15,precio);
+
+
+    }
+    @Test
+    void showVintageTest() {
+        Photo selfie = new DigitalPhoto();
+        Photo selfieVintage = new EffectVintage(selfie);
+        assertEquals("Normal photo without edit + Vintage", selfieVintage.show());
+        double precio = selfieVintage.cost();
+        assertEquals(30, precio);
+    }
+    @Test
+    void BrightnessTest() {
+        Photo selfie = new DigitalPhoto();
+
+        assertNull(selfie.getBrightness());
+       selfie.setBrightness(15);
+        int brightAfter= selfie.getBrightness();
+        assertEquals(15,brightAfter);
     }
 
     @Test
-    void getBrightness() {
+    void ContrastTest() {
+        Photo selfie = new DigitalPhoto();
+
+        assertNull(selfie.getContrast());
+        selfie.setContrast(25);
+        int contrastAfter= selfie.getContrast();
+        assertEquals(25,contrastAfter);
     }
 
     @Test
-    void getContrast() {
+    void SaturationTest() {
+        Photo selfie = new DigitalPhoto();
+
+        assertNull(selfie.getSaturation());
+        selfie.setSaturation(25);
+        int satAfter= selfie.getSaturation();
+        assertEquals(25,satAfter);
     }
 
-    @Test
-    void getSaturation() {
-    }
 
     @Test
-    void getBlur() {
+    void BlurTest() {
+        Photo selfie = new DigitalPhoto();
+
+        assertNull(selfie.getBlur());
+        selfie.setBlur(25);
+        int blurAfter= selfie.getBlur();
+        assertEquals(25,blurAfter);
     }
 
-    @Test
-    void setBrightness() {
-    }
 
-    @Test
-    void setContrast() {
-    }
-
-    @Test
-    void setSaturation() {
-    }
-
-    @Test
-    void setBlur() {
-    }
 }
